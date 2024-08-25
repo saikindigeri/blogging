b3dd441baee3a192da43201f50759adc88214240
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2024-08-25 12:58:15 +0530

    added name

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -16,7 +16,7 @@ const navigate=useNavigate()
       <div className="containerr">
 
         
-       <Link className='logo' href="/"> <h1 >MyBlog</h1></Link>
+       <Link className='logo' href="/"> <h1 >ZuAI</h1></Link>
         <nav>
           <ul className="nav-list">
             <li><Link to="/" className="nav-link">Home</Link></li>
